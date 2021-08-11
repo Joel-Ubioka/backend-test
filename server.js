d7945e4ad3a3b8382b36006a0e8fe29b8ddd2835
Joel Ubioka <66705253+morrhtech-solutions@users.noreply.github.com>
2021-08-11 14:51:03 -0700

    commiting first upload

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
+
+
+// Load env vars
+dotenv.config({ path: './config/config.env' });
+
+//connect to database
+connectDB();
+
+//Route files
+const bootcamps = require('./routes/bootcamps');
+
+const app = express();
+
+// body parser
+app.use(express.json());
+
+
+// dev loggin middleware
+if (process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'))
+}
+
+//Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+);
+
+// Handle unhandled promise rejections
+process.on('unhandledREjection', (err, promise) => {
+console.log(`Error: ${err.message}`.red);
+//close server and exit process
+server.close(() => process.exit(1));
+});
